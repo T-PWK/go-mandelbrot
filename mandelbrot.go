5829631bc692460556d2162967b2e305eca4d587
Tom Pawlak <pawlaktom@gmail.com>
2018-09-20 16:47:59 +0100

    Examples and unit test updates

--- mandelbrot.go
+++ mandelbrot.go
@@ -1,29 +1,30 @@
+// Package mandelbrot provides functions for generating a Mandelbrot set.
 package mandelbrot
 
 const (
-	// Min value for a default Mandelbrot fractal
+	// Min upper-left point for a default Mandelbrot fractal
 	Min = -2 + 1.4i
 
-	// Max value for a default Mandelbrot fractal
+	// Max lower-right point for a default Mandelbrot fractal
 	Max = 0.8 - 1.4i
 )
 
-// Mandelbrot returns an image of the Mandelbrot fractal of size (h,w)
-// between lu (left-upper) and rb (right-bottom) complex numbers with a specifed maximum number of iterations
-func Mandelbrot(lu, rb complex128, h, w, maxit uint) [][]uint {
+// Mandelbrot returns the Mandelbrot set of size (h,w).
+// The set is generated between ul (upper-left) and lr (lower-right) points with the maximum number of iterations.
+func Mandelbrot(ul, lr complex128, h, w, maxit uint) [][]uint {
 
 	if h < 2 || w < 2 {
 		panic("Invalid image size")
 	}
 
-	if real(lu) > real(rb) || imag(rb) > imag(lu) {
-		panic("Invalid complex numbers boundries")
+	if real(ul) > real(lr) || imag(lr) > imag(ul) {
+		panic("Invalid corner points")
 	}
 
-	rmin := real(lu)
-	imin := imag(rb)
-	dx := (real(rb) - real(lu)) / float64(w-1)
-	dy := (imag(lu) - imag(rb)) / float64(h-1)
+	rmin := real(ul)
+	imin := imag(lr)
+	dx := (real(lr) - real(ul)) / float64(w-1)
+	dy := (imag(ul) - imag(lr)) / float64(h-1)
 
 	c := make([][]uint, h)
 
@@ -40,13 +41,20 @@ func Mandelbrot(lu, rb complex128, h, w, maxit uint) [][]uint {
 	return c
 }
 
-// Iter calculates iteration value for a given complex number (point)
+// Iter calculates iteration value for a given number point.
+// The iteration value uses the following algorithm:
+//   z = p;
+//   repeat
+//     z = z*z + p
+//   until |z| < 2;
+// It returns number of iteration at which the |z| < 2 condition is not met or the maximum iterations value.
 func Iter(p complex128, maxit uint) uint {
 	z := p
 
 	for it := uint(0); it < maxit; it++ {
 		z = z*z + p
 
+		// For better performance we use r^2 + i^2 > 4 instead of cmplx.Abs(z) > 2
 		if r, i := real(z), imag(z); r*r+i*i > 4 {
 			return it
 		}
